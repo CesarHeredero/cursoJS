29c8e0d5cfc3c2f7ebf49234f8049385799d9c3d
CesarHeredero <heredero.cesar@gmail.com>
2020-01-20 20:32:37 +0100

    Update app.js
    diferentes formas de nombrar y de recibir los datos

--- objetos-constructores/app.js
+++ objetos-constructores/app.js
@@ -1,21 +1,101 @@
+const cliente = {
+    nombre: 'Juan',
+    saldo: 2000,
+    tipoCliente: function() {
+        let tipo;
+
+        if (this.saldo > 1000) {
+            tipo = 'Gold';
+        } else if (this.saldo > 500) {
+            tipo = 'platino';
+        } else {
+            tipo = 'Normal';
+        }
+        return tipo;
+    }
+}
+
+console.log(cliente.tipoCliente());
+
+// otra forma de nombrarlo
 function Cliente(nombre, saldo) {
     this.nombre = nombre;
     this.saldo = saldo;
     this.tipoCliente = function() {
         let tipo;
-        if (this.saldo > 10000) {
-            tipo = 'gold';
+        if (this.saldo > 1000) {
+            tipo = 'Gold2';
         } else if (this.saldo > 500) {
-            tipo = 'premium';
+            tipo = 'Platino2';
         } else {
-            tipo = 'normal';
+            tipo = ' Normal2';
         }
         return tipo;
     }
-
 }
-const persona1 = new Cliente('cesar', 1000000);
-const persona2 = new Cliente('rigoberto', 600);
-const persona3 = new Cliente('benito', 100);
 
-console.log(persona1, persona1.tipoCliente());
+const persona = new Cliente('Pedro', 20000);
+
+console.log(persona.tipoCliente());
+
+// String
+const nombre1 = 'Pedro';
+console.log(nombre1);
+console.log(typeof nombre1);
+
+const nombre2 = new String('Pedro');
+console.log(nombre2);
+console.log(typeof nombre2);
+
+if (nombre1 == nombre2) {
+    console.log('si, iguales');
+} else {
+    console.log('no son iguales')
+}
+
+if (nombre1 === nombre2) {
+    console.log('si, iguales');
+} else {
+    console.log('no son iguales')
+}
+
+// numeros 
+const numero1 = 1;
+const numero2 = new Number(1);
+
+console.log(numero1);
+console.log(numero2);
+
+// booleanos
+const verdadero = true;
+const verdadero2 = new Boolean(true);
+
+console.log(verdadero);
+console.log(verdadero2);
+
+// funciones 
+const funcion1 = function(a, b) {
+    return a + b;
+}
+
+const funcion2 = new Function('a', 'b', 'return 1 + 2');
+
+console.log(funcion1(2, 3));
+console.log(funcion2(1, 2));
+
+// Objetos
+const persona1 = {
+    nombre: 'juan',
+}
+
+const persona2 = new Object({ nombre: 'Juan', });
+
+console.log(persona1);
+console.log(persona2);
+
+// arreglos
+const arrglo1 = [1, 2, 3, 4];
+const arreglo2 = new Array(1, 2, 3, 4);
+
+console.log(arrglo1);
+console.log(arreglo2);
