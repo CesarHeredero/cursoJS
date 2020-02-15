2f6bd40562eb1fd80b700fab9373e0a885fba7c6
CesarHeredero <heredero.cesar@gmail.com>
2020-02-15 11:34:28 +0100

    ver presupuesto y restante

---
+++ gastoSemanal/js/app.js
@@ -0,0 +1,27 @@
+// Variables
+const presupuestoUsuaraio = prompt('Cual es tu presupuesto semanal');
+let cantidadPresuspuesto;
+
+
+// Clases
+class Presupuesto {
+    constructor(presupuesto) {
+        this.presupuesto = Number(presupuesto);
+        this.restante = Number(presupuesto);
+    }
+
+    presupuestoRestante(cantidad = 0) {
+        return this.restante -= Number(cantidad);
+    }
+}
+
+// Event Listeners
+document.addEventListener('DOMContentLoaded', function() {
+    if (presupuestoUsuaraio === null | presupuestoUsuaraio === '') {
+        window.location.reload();
+    } else {
+        // instanciar un presupuesto
+        cantidadPresuspuesto = new Presupuesto(presupuestoUsuaraio);
+        console.log(cantidadPresuspuesto);
+    }
+})
