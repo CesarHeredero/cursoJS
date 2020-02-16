56c68deabb7cbf19f5c8008dd27c514428feb041
CesarHeredero <heredero.cesar@gmail.com>
2020-02-16 13:41:28 +0100

    adsf

--- gastoSemanal/js/app.js
+++ gastoSemanal/js/app.js
@@ -1,27 +1,27 @@
-// Variables
-const presupuestoUsuaraio = prompt('Cual es tu presupuesto semanal');
-let cantidadPresuspuesto;
+// 15 // Variables
+// const presupuestoUsuaraio = prompt('Cual es tu presupuesto semanal');
+// let cantidadPresuspuesto;
 
 
-// Clases
-class Presupuesto {
-    constructor(presupuesto) {
-        this.presupuesto = Number(presupuesto);
-        this.restante = Number(presupuesto);
-    }
+// // Clases
+// class Presupuesto {
+//     constructor(presupuesto) {
+//         this.presupuesto = Number(presupuesto);
+//         this.restante = Number(presupuesto);
+//     }
 
-    presupuestoRestante(cantidad = 0) {
-        return this.restante -= Number(cantidad);
-    }
-}
+//     presupuestoRestante(cantidad = 0) {
+//         return this.restante -= Number(cantidad);
+//     }
+// }
 
-// Event Listeners
-document.addEventListener('DOMContentLoaded', function() {
-    if (presupuestoUsuaraio === null | presupuestoUsuaraio === '') {
-        window.location.reload();
-    } else {
-        // instanciar un presupuesto
-        cantidadPresuspuesto = new Presupuesto(presupuestoUsuaraio);
-        console.log(cantidadPresuspuesto);
-    }
-})
+// // Event Listeners
+// document.addEventListener('DOMContentLoaded', function() {
+//     if (presupuestoUsuaraio === null | presupuestoUsuaraio === '') {
+//         window.location.reload();
+//     } else {
+//         // instanciar un presupuesto
+//         cantidadPresuspuesto = new Presupuesto(presupuestoUsuaraio);
+//         console.log(cantidadPresuspuesto);
+//     }
+// })
