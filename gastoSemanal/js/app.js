f4be7d59b78a8df3b951742ef8975872ac82e0e2
CesarHeredero <heredero.cesar@gmail.com>
2020-02-18 08:34:07 +0100

    seguir con proyecto

--- gastoSemanal/js/app.js
+++ gastoSemanal/js/app.js
@@ -1,5 +1,6 @@
 // Variables
 const presupuestoUsuaraio = prompt('Cual es tu presupuesto semanal');
+const formulario = document.getElementById('agregar-gasto');
 let cantidadPresuspuesto;
 
 
@@ -15,6 +16,30 @@ class Presupuesto {
     }
 }
 
+class Interface {
+    insertarPresupuesto(cantidad) {
+        const presupuestoSpan = document.querySelector('span#total');
+        const restanteSpan = document.querySelector('span#restante');
+
+        presupuestoSpan.innerHTML = `${cantidad}`;
+        restanteSpan.innerHTML = `${cantidad}`;
+    }
+
+    imprimirMensaje(mensaje, tipo) {
+        const divMensaje = document.createElement('div');
+        divMensaje.classList.add('text-center', 'alert');
+        if (divMensaje === 'error') {
+            divMensaje.classList.add('alert-danger');
+        } else {
+            divMensaje.classList.add('alert-succes');
+        }
+
+        divMensaje.appendChild(document.createTextNode(mensaje));
+        //insertar en dom
+        document.querySelector('.primario').insertBefore(divMensaje, formulario);
+    }
+}
+
 // Event Listeners
 document.addEventListener('DOMContentLoaded', function() {
     if (presupuestoUsuaraio === null | presupuestoUsuaraio === '') {
@@ -22,6 +47,26 @@ document.addEventListener('DOMContentLoaded', function() {
     } else {
         // instanciar un presupuesto
         cantidadPresuspuesto = new Presupuesto(presupuestoUsuaraio);
-        console.log(cantidadPresuspuesto);
+        // intanciar clase interfaz
+        const ui = new Interface();
+        ui.insertarPresupuesto(cantidadPresuspuesto.presupuesto)
+    }
+})
+
+formulario.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    // leer del formulario de gasto
+    const nombreGasto = document.querySelector('#gasto').value;
+    const cantidadGasto = document.querySelector('#cantidad').value;
+
+    // instanciar la Interfaz
+    const ui = new Interfaz();
+
+    //comprobar que los campos no esten vacios
+    if (nombreGasto === '' || cantidadGasto === '') {
+        ui.imprimirMensaje('Hubo un error');
+    } else {
+        ui.imprimirMensaje('se agrego gasto');
     }
 })
