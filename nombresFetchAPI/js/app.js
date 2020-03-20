ff5b40ad3e31b6e3a47edbd2942dea794cf8df26
CesarHeredero <heredero.cesar@gmail.com>
2020-03-20 20:56:21 +0100

    Update app.js

--- nombresFetchAPI/js/app.js
+++ nombresFetchAPI/js/app.js
@@ -1,36 +1,54 @@
-
 document.querySelector('#generar-nombre').addEventListener('submit', cargarNombres);
 
 // Llamado a Ajax e imprimir resultados
 function cargarNombres(e) {
-     e.preventDefault();
+    e.preventDefault();
 
-     // Leer las variables
+    // Leer las variables
 
-     const origen = document.getElementById('origen');
-     const origenSeleccionado = origen.options[origen.selectedIndex].value;
+    const origen = document.getElementById('origen');
+    const origenSeleccionado = origen.options[origen.selectedIndex].value;
 
-     const genero = document.getElementById('genero');
-     const generoSeleccionado = genero.options[genero.selectedIndex].value;
+    const genero = document.getElementById('genero');
+    const generoSeleccionado = genero.options[genero.selectedIndex].value;
 
-     const cantidad = document.getElementById('numero').value;
+    const cantidad = document.getElementById('numero').value;
 
-    
 
-     let url = '';
-     url += 'http://uinames.com/api/?';
-     // Si hay origen agregarlo a la URL
-     if(origenSeleccionado !== '') {
-          url += `region=${origenSeleccionado}&`;
-     }
-     // Si hay un genero agregarlo a la URL
-     if(generoSeleccionado !== '') {
-          url += `gender=${generoSeleccionado}&`;
-     }
-     // Si hay una cantidad agregarlo a la URL
-     if(cantidad !== '') {
-          url += `amount=${cantidad}&`;
-     }
 
-     // Código de FETCH API AQUI
+    let url = '';
+    url += 'http://uinames.com/api/?';
+    // Si hay origen agregarlo a la URL
+    if (origenSeleccionado !== '') {
+        url += `region=${origenSeleccionado}&`;
+    }
+    // Si hay un genero agregarlo a la URL
+    if (generoSeleccionado !== '') {
+        url += `gender=${generoSeleccionado}&`;
+    }
+    // Si hay una cantidad agregarlo a la URL
+    if (cantidad !== '') {
+        url += `amount=${cantidad}&`;
+    }
+
+    // Código de FETCH API AQUI
+    fetch(url)
+        .then(function(res) {
+            return res.json();
+        })
+        .then(function(data) {
+            let html = '';
+            html += '<h1 style="color:black;"> Resulstado</h1><ul class="lista">';
+            data.forEach(function(nombre) {
+                html += `
+                <li>${nombre.name}</li>
+                `;
+            })
+            html += '</ul>';
+            document.getElementById('resultado').innerHTML = html;
+        })
+        .catch(function(error) {
+            console.log(error);
+        })
+
 }
